ee5a7340f499f43519d6b03ea80368f29212fe9e
Marc Boanas <techstacksdotco>
2021-07-08 16:02:48 +0100

    Initial Commit

---
+++ StudyGroup/StudyGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyGroupApp.swift
+//  StudyGroup
+//
+//  Created by Marc Boanas on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct StudyGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
